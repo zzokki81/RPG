6f028b5c63924077c75e4b1e9720e00dc79f6175
zzokki81 <zoran.rebic@outlook.com>
2020-09-23 23:26:48 +0200

    Add delete and edit account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
     @user = current_user
     @user_characters = @user.characters.order(:name).page(params[:page]).per(3)
   end
+
+  def destroy
+    @user = User.find(params[:id])
+    redirect_to(root_url, :notice => "User deleted.") if @user.destroy
+  end
 end
