d96efa2bc08f43d0e0075a6992daee61d426762e
zzokki81 <zoran.rebic@outlook.com>
2020-09-30 19:22:01 +0200

    Add current_user check

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,17 @@
 class UsersController < ApplicationController
+  before_action :correct_user
   def show
     @user = current_user
     @user_characters = @user.characters.order(:name).page(params[:page]).per(3)
   end
 
   def profile
-    respond_to do |format|
-      if User.exists?(params[:id])
-        @user = User.find(params[:id])
-        format.html
-      else
-        format.html { redirect_to root_path, notice: 'User does not exist!' }
-      end
+    @user = User.find(params[:id]) if User.exists?(params[:id])
+  end
+
+  def correct_user
+    unless current_user && params[:id].to_i == current_user.id
+      redirect_to(root_path, notice: "You don't have permission to see other users accounts!")
     end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,13 +1,14 @@
 require("rails_helper")
 RSpec.describe(UsersController, :type => :controller) do
-  user = FactoryBot.create(:user)
+  let(:user) { FactoryBot.create(:user) }
 
-  describe 'GET #profile' do
-    it("will find user") do
-      get :profile, params: {id: user.id}
+  describe "GET #profile" do
+    it("will find user info") do
+      sign_in(:user, user)
+      get :profile, params: {:id => user.id}
       expect(response.status).to be(200)
     end
-    it("will not find user") do
+    it("will not find user info") do
       get :profile, params: {id: 'foo'}
       expect(response).to redirect_to root_path
     end
